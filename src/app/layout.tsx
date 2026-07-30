c723e4ec22cc6c83acd1b14be3555721b266105c
Ainrane haddani <ainranehaddani@MacBook-Pro-de-Ainrane.local>
2026-07-30 18:15:43 +0100

    Add Google Tag Manager (GTM-TQH8BXBR)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -215,6 +215,15 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQH8BXBR');`,
+          }}
+        />
       </head>
       <body
         className="min-h-screen bg-white text-[#1e1e2a] antialiased"
@@ -226,6 +235,14 @@ export default function RootLayout({
           letterSpacing: "-0.005em",
         }}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TQH8BXBR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <BackToTop />
         <WhatsAppButton />
